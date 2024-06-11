63126f5370bfd63bc4aeb29cf7364d3efac02ff2
walter04guarac <130495513+walter04guarac@users.noreply.github.com>
2024-06-10 20:17:22 -0500

    k

--- 03-multForThree.js
+++ 03-multForThree.js
@@ -2,9 +2,15 @@
 
 function multiplicarPorTres(array) {
     for (let i = 0; i < array.length; i++) {
-        alert(array[i]+'x'+'3 ='+(array[i] * 3));
+        alert(array[i]+'x'+'3 = '+(array[i] * 3));
     }
 }
+let numero1=+prompt('Ingrese un numero',0);
+let numero2=+prompt('Ingrese un numero',0);
+let numero3=+prompt('Ingrese un numero',0);
+let numero4=+prompt('Ingrese un numero',0);
 
-let numeros = [1, 2, 3, 4];
+
+
+let numeros = [numero1, numero2, numero3, numero4];
 multiplicarPorTres(numeros);
